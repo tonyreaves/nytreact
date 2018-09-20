4bab4fb05f2a59f6a3fee083eae8b99d0c826ac2
tonyreaves <tonyfreaves@gmail.com>
2018-09-19 20:35:06 -0700

    got NYT api key

--- src/App.js
+++ src/App.js
@@ -15,26 +15,25 @@ class App extends Component {
         <Results />
       </div>
     );
-  };
+  }
 }
 
 // NYTimes API
-const queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-  show + "&api_key=GDrp9yYXxOT08SiDuSbymTZKPxz747Yg&limit=10";
+const queryURL =
+  "https://api.nytimes.com/svc/search/v2/articlesearch.json" +
+  show +
+  "&api-key=26c173d356d24490a332df2068d7f56f&q=";
 
 // AJAX GET
 $.ajax({
   url: queryURL,
   method: "GET"
-})
+}).then(function(response) {
+  var results = response.data;
 
-.then(function(response) {
-
-    var results = response.data;
-
-    // For loop to grab 10 images
-//     for (var i = 0; i < results.length; i++) {
-// }
-}
+  // For loop to grab 10 images
+  //     for (var i = 0; i < results.length; i++) {
+  // }
+});
 
 export default App;
