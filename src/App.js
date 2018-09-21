efb40803ee414b3b4a00037e3074669f81d48b09
tonyreaves <tonyfreaves@gmail.com>
2018-09-20 19:48:04 -0700

    created article model

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./Components/Header/Header.js";
 import Search from "./Components/Search/Search.js";
 import Results from "./Components/Results/Results.js";
 import Saved from "./Components/Saved/Saved.js";
+import searchApi from "./Utils/API.js";
 
 class App extends Component {
   render() {
@@ -21,22 +22,6 @@ class App extends Component {
   }
 }
 
-let myQuery = "";
-let beginYear = "";
-let endYear = "";
-
-// NYTimes API
-const queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-queryURL +=
-  "?" +
-  $.param({
-    "api-key": "26c173d356d24490a332df2068d7f56f",
-    q: myQuery,
-    begin_date: beginYear + "0101",
-    end_date: endYear + "1231",
-    page: 0
-  });
-
 class MyComponent extends React.Component {
   constructor(props) {
     super(props);
@@ -45,26 +30,26 @@ class MyComponent extends React.Component {
     };
   }
 
-  componentDidMount() {
-    fetch(queryURL + myQuery)
-      .then(res => res.json())
-      .then(
-        result => {
-          this.setState({
-            isLoaded: true,
-            headline: news.headline.main,
-            weburl: news.web_url
-          });
-        },
+  // componentDidMount() {
+  //   fetch(BASEURL + myQuery)
+  //     .then(res => res.json())
+  //     .then(
+  //       result => {
+  //         this.setState({
+  //           isLoaded: true,
+  //           headline: news.headline.main,
+  //           weburl: news.web_url
+  //         });
+  //       },
 
-        error => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      );
-  }
+  //       error => {
+  //         this.setState({
+  //           isLoaded: true,
+  //           error
+  //         });
+  //       }
+  //     );
+  // }
 }
 
 export default App;
