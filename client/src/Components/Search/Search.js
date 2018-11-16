c300b9d3ce3b1fe9eea71cf59a56cc72d460890a
tonyreaves <tonyfreaves@gmail.com>
2018-11-15 20:40:51 -0700

    submit button working

--- client/src/Components/Search/Search.js
+++ client/src/Components/Search/Search.js
@@ -1,43 +1,56 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Search.css";
 import searchApi from "../../Utils";
-console.log(
-  searchApi.searchApi({
-    query: "William P. Davis",
-    beginYear: "1991",
-    endYear: "2018"
-  })
-);
 
-const Search = props => (
-  <div className="searchBox">
-    <h2>Search</h2>
-    <form>
-      <label for="topic">Topic</label>
-      <p>
-        <input className="searchField" type="text" name="topic" id="topic" />
-      </p>
-      <label for="startYear">Start Year</label>
-      <p>
-        <input
-          className="searchField"
-          type="text"
-          name="startYear"
-          id="startYear"
-        />
-      </p>
-      <label for="endYear">End Year</label>
-      <p>
-        <input
-          className="searchField"
-          type="text"
-          name="endYear"
-          id="endYear"
-        />
-      </p>
-      <button id="search">Search</button>
-    </form>
-  </div>
-);
+export default class Search extends Component {
+  onSubmit = event => {
+    event.preventDefault();
+    console.log(
+      searchApi.searchApi({
+        query: "William P. Davis",
+        beginYear: "1991",
+        endYear: "2018"
+      })
+    );
+  };
 
-export default Search;
+  render() {
+    return (
+      <div className="searchBox">
+        <h2>Search</h2>
+        <form>
+          <label htmlFor="topic">Topic</label>
+          <p>
+            <input
+              className="searchField"
+              type="text"
+              name="topic"
+              id="topic"
+            />
+          </p>
+          <label htmlFor="startYear">Start Year</label>
+          <p>
+            <input
+              className="searchField"
+              type="text"
+              name="startYear"
+              id="startYear"
+            />
+          </p>
+          <label htmlFor="endYear">End Year</label>
+          <p>
+            <input
+              className="searchField"
+              type="text"
+              name="endYear"
+              id="endYear"
+            />
+          </p>
+          <button onClick={event => this.onSubmit(event)} id="search">
+            Search
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
