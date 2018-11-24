254ab60cf3365deafeec66f519a7fddf03bdb9f8
tonyreaves <tonyfreaves@gmail.com>
2018-11-23 20:22:15 -0700

    got results into individual boxes

--- client/src/Components/Results/Results.js
+++ client/src/Components/Results/Results.js
@@ -2,8 +2,19 @@ import React from "react";
 import "./Results.css";
 
 const Results = props => (
-  <div className="searchBox">
-    <h2>Results</h2>
+  <div>
+    <div className="searchBox">
+      <h2>Results</h2>
+    </div>
+    {props.items.length > 0 && (
+      <ul>
+        <li>
+          {props.items.map(result => (
+            <div className="searchBox"> {result.headline.main} </div>
+          ))}
+        </li>
+      </ul>
+    )}
     {/* // for (let i = 0; i < 5; i++) {
 //   Articles = <a href={this.news.headline.main}
 // } */}
--- client/src/Components/Search/Search.js
+++ client/src/Components/Search/Search.js
@@ -1,55 +1,72 @@
 import React, { Component } from "react";
 import "./Search.css";
 import searchApi from "../../Utils";
+import Results from "../Results/Results.js";
 
 export default class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      results: []
+    };
+  }
+  //onSubmit should store the results in the state, then show results component if results are present
+  //searchApi func returns a promise, so to access data will need .then
+
   onSubmit = event => {
     event.preventDefault();
-    console.log(
-      searchApi.searchApi({
+    searchApi
+      .searchApi({
         query: "William P. Davis",
         beginYear: "1991",
         endYear: "2018"
       })
-    );
+      .then(({ data }) => {
+        this.setState({ results: data.response.docs });
+      });
   };
 
   render() {
     return (
-      <div className="searchBox">
-        <h2>Search</h2>
-        <form>
-          <label htmlFor="topic">Topic</label>
-          <p>
-            <input
-              className="searchField"
-              type="text"
-              name="topic"
-              id="topic"
-            />
-          </p>
-          <label htmlFor="startYear">Start Year</label>
-          <p>
-            <input
-              className="searchField"
-              type="text"
-              name="startYear"
-              id="startYear"
-            />
-          </p>
-          <label htmlFor="endYear">End Year</label>
-          <p>
-            <input
-              className="searchField"
-              type="text"
-              name="endYear"
-              id="endYear"
-            />
-          </p>
-          <button onClick={event => this.onSubmit(event)} id="search">
-            Search
-          </button>
-        </form>
+      <div>
+        <div className="searchBox">
+          <h2>Search</h2>
+          <form>
+            <label htmlFor="topic">Topic</label>
+            <p>
+              <input
+                className="searchField"
+                type="text"
+                name="topic"
+                id="topic"
+              />
+            </p>
+            <label htmlFor="startYear">Start Year</label>
+            <p>
+              <input
+                className="searchField"
+                type="text"
+                name="startYear"
+                id="startYear"
+              />
+            </p>
+            <label htmlFor="endYear">End Year</label>
+            <p>
+              <input
+                className="searchField"
+                type="text"
+                name="endYear"
+                id="endYear"
+              />
+            </p>
+            <button onClick={event => this.onSubmit(event)} id="search">
+              Search
+            </button>
+          </form>
+        </div>
+        <div>
+          <Results items={this.state.results} />
+        </div>
       </div>
     );
   }
